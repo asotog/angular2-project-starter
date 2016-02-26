ba05740cf34c60b9cde4a15b16cf97fa3731c610
Alejandro Soto <alejandro.soto@rivetlogic.com>
2016-02-26 09:51:09 -0600

    angular updated to latest beta 7

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -1,3 +1,6 @@
+///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+
+
 import {bootstrap}    from 'angular2/platform/browser';
 import {enableProdMode}    from 'angular2/core';
 import {HeroListComponent} from './heroes/hero-list.component';
